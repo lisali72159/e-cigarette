3674dc996c9f4fefa6714d893252de4ea8b784e5
Lisa <lisa.li72159@gmail.com>
2019-10-15 17:47:30 -0400

    Display data in console

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import './styles/index.scss';
 
+
+var tobacco_data1 = require('../data/tobacco_use.json');
+
 var width = 700;
 var height = 580;
 
@@ -8,8 +11,12 @@ var svg = d3.select("body")
     .attr("width", width)
     .attr("height", height);
 
+// var tobacco_data = [];
+// $.each(tobacco_data.y)
+
+
 window.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('app').innerText = "Hello World!";
+document.getElementById('app').innerText = "Hello World!";
 
 
 
@@ -18,5 +25,8 @@ window.addEventListener('DOMContentLoaded', () => {
 
 // Append empty placeholder g element to the SVG
 // g will contain geometry elements
-// var g = svg.append("g");
+var g = svg.append("g");
 
+console.log('hello');
+console.log(Object.values(tobacco_data1)[0].data_value);
+console.log('finish');
