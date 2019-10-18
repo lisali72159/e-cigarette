78cf3053dcf8ac02c2da9e4d50974970e22a1841
App Academy Student <jc4883@columbia.edu>
2019-10-18 17:52:26 -0400

    Created axis for bar graph

--- src/bar.js
+++ src/bar.js
@@ -54,30 +54,60 @@ var dataset = [
   4.8
 ];
 
-var svgWidth = 900,
-  svgHeight = 90,
-  barPadding = 0;
+var svgWidth = 1000,
+  svgHeight = 500,
+  barPadding = 4;
+
 var barWidth = svgWidth / dataset.length;
 
-var svg = d3.select("body")
+
+var svg = d3
+  .select("body")
   .append("svg")
   .attr("width", svgWidth)
   .attr("height", svgHeight)
   .attr("class", "bar-chart")
+  .attr("fill", "steelblue")
 
-// var g = svg.append("g");
+ 
+var xScale = d3
+    .scaleLinear()
+    .domain([0, d3.max(dataset)])
+    .range([0, svgWidth]);
 
+var yScale = d3
+  .scaleLinear()
+  .domain([0, d3.max(dataset)])
+  .range([svgHeight, 0]);
+
+  var x_axis = d3.axisBottom().scale(xScale);
+
+  var y_axis = d3.axisLeft().scale(yScale);
+         
+svg
+  .append("g")
+  .attr("transform", "translate(50, 2)")
+  .call(y_axis);
+
+var xAxisTranslate = svgHeight - 20;
+
+  svg
+    .append("g")
+    .attr("transform", "translate(0, " + xAxisTranslate + ")")
+    .call(x_axis);
+         
 var barChart = svg
   .selectAll("rect")
   .data(dataset)
   .enter()
   .append("rect")
   .attr("y", function(d) {
-    return svgHeight - d;
+    return yScale(d) ;
   })
   .attr("height", function(d) {
-    return d;
+    return svgHeight - yScale(d);
   })
+  .attr("transform", "translate(10)")
   .attr("width", barWidth - barPadding)
   .attr("transform", function(d, i) {
     var translate = [barWidth * i, 0];
